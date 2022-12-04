b2034e1073124e0f9950276e9335aec2cbb15be1
11889254Amaal <amalaljasiri@gmail.com>
2022-12-04 15:47:50 +0400

    12/4/2022 update

--- src/FirstPackage/Main.java
+++ src/FirstPackage/Main.java
@@ -15,19 +15,24 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Scanner;
 
+
+
+
 public class Main {
 	private static final String filepath = "C:\\Users\\Amaal\\Downloads\\AmaalAljasria\\EvaluationProject\\src\\eva.txt";
-
+	private static final String pathFile1="C:\\Users\\Amaal\\Downloads\\AmaalAljasria\\EvaluationProject\\src\\eva1.txt";
 	static void choicesFunction() {
 		// this fuction to pearrs for user in switch case
 		System.out.println("***************************");
 		System.out.println("Welcome...Please Select one of the following options:");
 		System.out.println("1. Consume Api and add to file");
 		System.out.println("2. Search from File");
-		System.out.println("4. take value from josn file");
+		System.out.println("4. take value from josn file and Translate");
 		System.out.println("3. Exit the system ");
 	}
 //main -====
+	
+	
 	public static void main(String[] args) throws Throwable {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
@@ -74,59 +79,28 @@ public class Main {
 				// this case to search from file
 				System.out.println("========================= ( Search From json file)===================");
 				System.out.println("===================================================================");
-//				HashSet<String> uniqueNames = new HashSet<String>();
-//				ArrayList<String> outputLines = new ArrayList<String>();
-//
-//				Scanner input = new Scanner(
-//						new File("C:\\Users\\Amaal\\Downloads\\AmaalAljasria\\EvaluationProject\\src\\eva.txt"));
-//				while (input.hasNextLine()) {
-//
-//					// Split a line into an array of names.
-//					String[] names = input.nextLine().split(" ");
-//					String edited = "";
-//					for (int i = 0; i < names.length; i++) {
-//
-//						// If the name is already in the set, remove it.
-//						if (uniqueNames.add(names[i])) {
-//							edited += names[i] + " ";
-//
-//							char char1 = edited.charAt(i);
-//
-//							if ((char1 >= 'a' && char1 <= 'z') || (char1 >= 'A' && char1 <= 'Z')
-//									|| (Character.isDigit(char1))) {
-//								System.out.println("yas");
-//								
-//								edited = edited.strip(); // remove excess whitespace
-//
-//								// Add the final line to our output array.
-//								if (!edited.equals("")) {
-//									outputLines.add(edited);
-//								}
-//								String outputFn = "notJsonFile.txt";
-//								BufferedWriter output = new BufferedWriter(new FileWriter(outputFn));
-//								output.write(char1);
-//								output.close();
-//								System.out.println("File '" + outputFn + "' created!");
-//							}
-//
-//						}
-//					}
-//				
-//				}
 
-//				// Write the output array to a file.
-//				String outputFn = "withOutDuplicate.txt";
-//				BufferedWriter output = new BufferedWriter(new FileWriter(outputFn));
-//				output.write(String.join("\n", outputLines));
-//				output.close();
-//				System.out.println("File '" + outputFn + "' created!");
-
-				/////////////////////////////////////////////////////////////////
-				
 				
 				System.out.println("plz enter word that want to search from file");
-       		 Scanner sc1=new Scanner(System.in);
+       		   Scanner sc1=new Scanner(System.in);
 	      	    String SearchForWord2= sc1.next();
+	      	  System.out.print("Please enter the number of strings you want to enter: ");   
+	      	//takes an integer input         
+	      	String[] string = new String [sc.nextInt()];      
+	      	//consuming the <enter> from input above  
+	      	sc.nextLine();   
+	      	for (int i = 0; i < string.length; i++)   
+	      	{  
+	      	string[i] = sc.nextLine();  
+	      	}  
+	      	System.out.println("\nYou have entered: ");  
+	      	//for-each loop to print the string  
+	      	for(String str: string)   
+	      	{  
+	      	
+	      	System.out.println(str);  
+	      	}  
+	      	  
 	      	  File f1=new File("C:\\Users\\Amaal\\Downloads\\AmaalAljasria\\EvaluationProject\\src\\eva.txt"); //Creation of File Descriptor for input file
 	          String[] words=null;  
 	          FileReader fr = new FileReader(f1);  //Creation of File Reader object
@@ -134,21 +108,18 @@ public class Main {
 	          String s;     
 	          
 	          int count=0;   
-				
+			 String s1;
 	          while((s=br1.readLine())!=null)   //Reading Content from the file
 	          {
+	        	 s1=s.replaceAll("\\W", " ");
 	             words=s.split(" ");  //Split the word using space
 	              for (String word : words) 
 	              {
-	                     if (word.equals(SearchForWord2))   //Search for the given word
+	                     if (word.contains(SearchForWord2))   //Search for the given word
 	                     {
 	                    	 count++; 
-	                    	 System.out.println("******the word found********");
 	                     }
-	                     else
-	                     {
-	                    	 System.out.println("******the word not found********");
-	                     }
+	               
 				
 	              }
 	          }
@@ -167,40 +138,18 @@ public class Main {
 	               
 	          
 	          
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				HashSet<String> lines = new HashSet<String>();
+		             choicesFunction();
 
-				File file = new File("C:\\Users\\Amaal\\Downloads\\AmaalAljasria\\EvaluationProject\\src\\txt1.txt");
 				
-				Scanner input1 = new Scanner(file);
-				int count1 = 0;
-				System.out.println("All words in file");
-				System.out.println("=================");
-				while (input1.hasNext()) {
-					String next = input1.next();
-					
-					
-					
-					System.out.println(next);
-					
-					lines.add(next);
-					count1++;
-					
-				}
-				System.out.println("=================");
-				input1.close();
-				System.out.println("word without duplicate" + lines);
-				System.out.println("Word count: " + count1);
+				
+				
+				
+				
+		           
 
-				choicesFunction();
+				
+				
+		
 
 				break;
 			case 3:
@@ -210,12 +159,47 @@ public class Main {
 				break;
 			case 4:
 				
-				File file1 = new File("C:\\Users\\Amaal\\Downloads\\AmaalAljasria\\EvaluationProject\\src\\eva.txt");
-				Scanner in=new Scanner(file1);
-				while((in.hasNextLine())) {
-					System.out.println(in.next().replaceAll("[^a-zA-Z]*", ""));
+	
+				
+				
+				File f11=new File("C:\\Users\\Amaal\\Downloads\\AmaalAljasria\\EvaluationProject\\src\\eva.txt"); //Creation of File Descriptor for input file
+		          String[] words1=null;  
+		          FileReader fr1 = new FileReader(f11);  //Creation of File Reader object
+		          BufferedReader br11 = new BufferedReader(fr1); //Creation of BufferedReader object
+
+		          
+		          int count11=0;   
 					
-				}
+		          while((s1=br11.readLine())!=null)   //Reading Content from the file
+		          {
+		             words1=s1.split(" ");  //Split the word using space
+		             System.out.println("==================================================");
+		              for (String word : words1) 
+		              {
+		            	  //System.out.println(word);
+		                     if (word.contains(":"))   //Search for the given word
+		                     {
+		                    	String a1= word.replaceAll("\\W", " ");
+		                    	
+		                    	
+		                    	System.out.println(a1);
+		                    	
+		                    	
+		    					
+		                     }
+		                     Path pathFile = Paths.get(pathFile1);
+		    					Files.writeString(pathFile, word.replaceAll("\\W", " "), StandardCharsets.UTF_8);
+		                 
+					
+		                     
+		              }
+		              System.out.println("==================================================");
+		              System.out.println("=========================Json File Translate========================");
+		          }
+		             
+			             fr1.close();
+		                	
+			             choicesFunction();
 				break;
 			// code block
 			}
